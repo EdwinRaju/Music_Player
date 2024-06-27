da174f583531cb6f7a3d402abb403e06164ee8aa
edwin <edwinraju2000@gmail.com>
2024-06-27 12:39:15 +0530

    Home page is setup

--- musicplayer/frontend/urls.py
+++ musicplayer/frontend/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from .views import index
+
 urlpatterns = [
-path('', index, name='index'),
+    path('', index),
+    path('join', index),
+    path('create', index),
+    path('join/1', index)
 ]
