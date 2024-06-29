cd7b89cf5c14879e9184b199e9343b1f5146d567
edwin <edwinraju2000@gmail.com>
2024-06-29 09:52:53 +0530

    Handling POST Requests

--- musicplayer/api/models.py
+++ musicplayer/api/models.py
@@ -12,7 +12,7 @@ def generate_uniquie_code():
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_uniquie_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
